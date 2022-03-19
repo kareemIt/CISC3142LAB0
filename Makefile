371a949bc77f84b0cd1267a22f8684d756bc0f6b
karee <karee@DESKTOP-HCGI81U>
2022-03-19 17:48:59 -0400

    added comments and updated makefile

--- Makefile
+++
@@ -1,4 +0,0 @@
-lab1: lab1/file1.o
-	g++ ./lab1/file1.cpp -o ./lab1/file1 -std=c++98
-	cd lab1 && ./file1
-lab2:
--- lab2/file.cpp
+++ lab2/file.cpp
@@ -6,14 +6,16 @@ struct Calculator {
   double firstNum;
   double secondNum;
   int userMode;
+  //keeps tracks of the operations as enums with set values
   enum Operation { Add = 1, Subtract = 2, Multiply = 3, Divide = 4, Sqrt = 5,Exponent = 6 };
 
+//takes in the user inputs and sets them to local variables for doing operartions
   Calculator(double firstInput,double secondInput,int modeInput){
       firstNum = firstInput;
       secondNum = secondInput;
       userMode = modeInput;
   }
-
+//Takes in the user inputed mode then chooses the right action depending on what they put in or default to -1
   int calculate(int userMode) {
       switch (userMode){
       case Operation::Add:
@@ -46,6 +48,7 @@ int main() {
     double secondInput;
     int mode;
 
+    //displays options for the users and takes in user input
     cout << "Calculator options:\n Addtion: 1  \n Subtractions: 2 \n Multiply: 3 \n Divide: 4 \n Square Root: 5 \n Exponent: 6 ";
     cin >> mode; 
     cout << "Input your first number \n";
@@ -55,6 +58,7 @@ int main() {
     cin >> secondInput;
     }
     Calculator answer = Calculator(firstInput,secondInput,mode);
+    //displays the answer for the user
     cout << answer.calculate(mode);
     return 0;
 }
